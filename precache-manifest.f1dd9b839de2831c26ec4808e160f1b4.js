e855de5bd5a77af4672646550cdb6399f9c5cba7
weizhexin <1315685142@qq.com>
2020-06-08 23:05:19 +0800

    deploy

---
+++ precache-manifest.f1dd9b839de2831c26ec4808e160f1b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f4559e8d33e8010dd6",
+    "url": "/money-website/css/app.bc1821ee.css"
+  },
+  {
+    "revision": "d0829786f2b73b2ce6fccc2531442f0b",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "96f4559e8d33e8010dd6",
+    "url": "/money-website/js/app.007191d3.js"
+  },
+  {
+    "revision": "636b6e823e88f629f65a",
+    "url": "/money-website/js/chunk-vendors.0ed2e99a.js"
+  },
+  {
+    "revision": "5a3bc337ed9627c080692ef3c952b5ba",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "52f43aa3d3a9ba84b844028da4486be5",
+    "url": "/money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
